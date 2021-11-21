a39a55535579dc0971bfb68ab453cc6ddc060371
lukaszkus <kusiu@vp.pl>
2021-11-21 17:01:42 +0100

    basic configuration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import './styles/main.scss';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Earth</h1>
+      <h2>Venus</h2>
+      <h3>Jupiter & Saturn</h3>
+      <h4>Uranus, Neptune & Pluto</h4>
+      <h5>So, you want to travel to space</h5>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. 
+      </p>
+    </>
   );
 }
 
