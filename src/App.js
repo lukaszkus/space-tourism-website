b64ae56fcdf9ec3452f43c75069892f1e900fd88
lukaszkus <kusiu@vp.pl>
2021-11-24 21:27:44 +0100

    basic styles update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './styles/main.scss';
 
 function App() {
   return (
-    <>
+    <div class="container">
       <h1>Earth</h1>
       <h2>Venus</h2>
       <h3>Jupiter & Saturn</h3>
@@ -11,7 +11,7 @@ function App() {
       <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. 
       </p>
-    </>
+    </div>
   );
 }
 
