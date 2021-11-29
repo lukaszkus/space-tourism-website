9b037ca90bec8f1ae8d8fce7c1fcfd577d4ae9fb
lukaszkus <kusiu@vp.pl>
2021-11-29 14:41:30 +0100

    home page styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,28 @@
 import * as React from "react";
 
-import './Home.scss';
+import "./Home.scss";
 
 export default function Home() {
   return (
     <>
-      <main className="container">
-        <h2>Home</h2>
-        <p>You can do this, I believe in you.</p>
+      <main className="home grid-container">
+        <div className="col-left">
+          <h1 className="title">
+            So, you want to travel to
+            <span className="title--decoration">Space</span>
+          </h1>
+          <p className="home__txt">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div class="col-right">
+          <a href="/" className="cta">
+            Explore
+          </a>
+        </div>
       </main>
     </>
   );
